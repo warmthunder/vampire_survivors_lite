77189d666b97c0c68cff7497f930596b25fefd04
warmthunder <hriday.singh99@gmail.com>
2026-02-06 15:53:08 +0530

    enemies, shooting, progression

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ canvas.height = innerHeight;
 
 const c = canvas.getContext('2d');
 
+let gameover = false;
+
+
+
 let mousex = 0;
 let mousey = 0;
 let bullet_s = 3;
@@ -12,6 +16,8 @@ let bullet = false;
 let bullet_c = 0;
 let bullets = [];
 let enemies = [];
+let enemytime = 1000;
+
 
 let pdist = 0;
 let hdist = 0;
@@ -20,8 +26,14 @@ let vdist = 0;
 const player = new Player_Circle(innerWidth/2,innerHeight/2);
 enemies.push(new Enemy());
 
-let keys = [false, false, false, false, false];
+let keys = [false, false, false, false];
 
+window.addEventListener('click',function(){
+    
+    
+    bullets.push(new Bullet_Circle(player.x,player.y,3));
+
+})
 
 window.addEventListener('keydown', function(event){
 
@@ -42,10 +54,6 @@ if(event.key == 'd'){
 
 }
 
-if(event.key == ' '){
-    
-    bullets.push(new Bullet_Circle(player.x,player.y,5));
-}
 })
 
 window.addEventListener('keyup', function(event){
@@ -85,19 +93,19 @@ function Player_Circle(x,y){
     this.draw = function(){
         c.beginPath();
         c.fillStyle = '#1C646D'
-        c.arc(this.x,this.y,75,0,6.28);
+        c.arc(this.x,this.y,30,0,6.28);
         c.fill();
     }
 
     this.update = function(){
-        if(keys[0]==true)
-            this.y-=2;
-        if(keys[1]==true)
-            this.y+=2;
-        if(keys[2]==true)
-            this.x-=2;
-        if(keys[3]==true)
-            this.x+=2;
+        if(keys[0]==true && this.y>0)
+            this.y-=10;
+        if(keys[1]==true && this.y<innerHeight)
+            this.y+=10;
+        if(keys[2]==true && this.x>0)
+            this.x-=10;
+        if(keys[3]==true && this.x<innerWidth)
+            this.x+=10;
         
 
 this.draw();
@@ -106,6 +114,8 @@ this.draw();
 
 
 }
+
+
 
 function Bullet_Circle(x,y,r){
     this.x = x;
@@ -118,7 +128,7 @@ function Bullet_Circle(x,y,r){
 
     this.draw = function(){
         c.beginPath();
-        c.fillStyle = '#ffffffff'
+        c.fillStyle = '#CEF09D'
         c.arc(this.x,this.y,r,0,6.28);
         c.fill();
     }
@@ -126,8 +136,8 @@ function Bullet_Circle(x,y,r){
     this.update = function(){ 
      if(this.bullet){   
     this.pdist = Math.sqrt((player.x-mousex)**2 + (player.y-mousey)**2);
-    this.dx = 3*(mousex-x)/this.pdist;
-    this.dy = 3*(mousey-y)/this.pdist;
+    this.dx = 14*(mousex-x)/this.pdist;
+    this.dy = 14*(mousey-y)/this.pdist;
       this.bullet = false;
     }
 
@@ -149,13 +159,31 @@ function Bullet_Circle(x,y,r){
 }
 
 function Enemy(){
+    
+    this.whatiszero = Math.random()-0.5;
+
+
+
     this.x = Math.random()*innerWidth;
     this.y = Math.random()*innerHeight;
-
+//only spawn in the edge
+    if(this.whatiszero>0)
+    {   if(this.whatiszero>0.25)
+        this.x = 0;
+        else
+            this.x = innerWidth;
+    }
+        else{
+            if(this.whatiszero<-0.25)
+        
+            this.y = 0;
+            else
+                this.y = innerHeight;
+        }
     this.draw = function(){
         c.beginPath();
         c.fillStyle = '#704115'
-        c.arc(this.x,this.y,30,0,6.28);
+        c.arc(this.x,this.y,15,0,6.28);
         c.fill();
     }
 
@@ -181,41 +209,88 @@ function detect(x1,y1,r1,x2,y2,r2){
 
 }
 
-let enemy = new Enemy();
+
+//spawn enemies
+setInterval(() => {
+ enemies.push(new Enemy())   
+}, enemytime);
+
+
 
 function animate(){
+    //clear everything
+    c.clearRect(0,0,innerWidth,innerHeight);
+if(!gameover)
+{
 
-//clear everything
-c.clearRect(0,0,innerWidth,innerHeight);
 
 //make player
 
 player.update();
 
+
+
+
 //enemy
-if(enemies[0])
-enemies[0].update();
-
-
-
-for(let i =0; i<bullets.length;i++){
-if(bullets[i])
-{   
-    bullets[i].update();
-    if(enemies[0]&&detect(bullets[i].x, bullets[i].y, bullets[i].r, enemies[0].x,enemies[0].y, 30))
+if(enemies.length>0){
+    for(let i = 0; i<enemies.length;i++){
+    enemies[i].update();
+    if (detect(player.x, player.y, 30, enemies[i].x,enemies[i].y, 15))
     {
-        enemies.splice(0,1);
-        bullets.splice(i,1);
+    gameover = true;
+}
+    }
+
+}
+
+
+
+for(let i = bullets.length -1; i >=0 ;i--){
+    
+if(bullets!= [])
+{   
+    if((bullets[i].x>innerWidth || bullets[i].x<0 || bullets[i].y>innerHeight || bullets[i].y<0))
+{   
+    bullets.splice(i,1);
+    continue;
+}
+    bullets[i].update();
+    for(let a =enemies.length -1;a>=0;a--){
+    if(detect(bullets[i].x, bullets[i].y, bullets[i].r, enemies[a].x,enemies[a].y, 15 ))
+    {   
+        
+        enemies.splice(a,1);
+        bullets.splice(i,1);   
+        break;
+        
 
     }
     
 }
-if(bullets[i] && (bullets[i].x>innerWidth || bullets[i].x<0 || bullets[i].y>innerHeight || bullets[i].y<0))
-{
-    bullets.splice(i,1);
+    
 }
+
+
+
 }
+
+enemytime -=0.1;
     requestAnimationFrame(animate);
+}
+
+else{
+    c.fillStyle = "#CEF09D"
+c.textAlign = 'center';
+c.font = "80px Arial"
+c.fillText("GAME OVER",innerWidth/2,innerHeight/2)
+
+c.textAlign = 'center';
+c.font = "30px Arial"
+c.fillText("Restart to Play Again",innerWidth/2 ,innerHeight/2+ 50)
+
+
+}
+
 }
 
 animate();
