2e4f7cedf1b9d9ee775811cbaa3918a8fa70e6db
warmthunder <hriday.singh99@gmail.com>
2026-02-07 20:46:10 +0530

    added enemies, shooters, lives

--- src/main.js
+++ src/main.js
@@ -11,27 +11,33 @@ let gameover = false;
 
 let mousex = 0;
 let mousey = 0;
-let bullet_s = 3;
 let bullet = false;
-let bullet_c = 0;
 let bullets = [];
+let enemyBullets = [];
 let enemies = [];
-let enemytime = 1000;
-
+let enemytime = 120;
+let is_hit = 0;
+let shoot = 120;
+let enemyCount = 0;
 
 let pdist = 0;
 let hdist = 0;
 let vdist = 0;
 
 const player = new Player_Circle(innerWidth/2,innerHeight/2);
-enemies.push(new Enemy());
+enemies.push(new Enemy('#704115'));
 
 let keys = [false, false, false, false];
 
 window.addEventListener('click',function(){
     
-    
-    bullets.push(new Bullet_Circle(player.x,player.y,3));
+    bullets.push(new Bullet_Circle(player.x,player.y,3, mousex, mousey));
+
+})
+
+window.addEventListener('resize', ()=>{
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
 })
 
@@ -88,6 +94,7 @@ mousey = event.clientY;
 function Player_Circle(x,y){
     this.x = x;
     this.y = y;
+    this.hp = 5;
     
 
     this.draw = function(){
@@ -99,13 +106,13 @@ function Player_Circle(x,y){
 
     this.update = function(){
         if(keys[0]==true && this.y>0)
-            this.y-=10;
+            this.y-=5;
         if(keys[1]==true && this.y<innerHeight)
-            this.y+=10;
+            this.y+=5;
         if(keys[2]==true && this.x>0)
-            this.x-=10;
+            this.x-=5;
         if(keys[3]==true && this.x<innerWidth)
-            this.x+=10;
+            this.x+=5;
         
 
 this.draw();
@@ -117,7 +124,7 @@ this.draw();
 
 
 
-function Bullet_Circle(x,y,r){
+function Bullet_Circle(x,y,r, x2, y2){
     this.x = x;
     this.y = y;
     this.r = r;
@@ -135,9 +142,9 @@ function Bullet_Circle(x,y,r){
 
     this.update = function(){ 
      if(this.bullet){   
-    this.pdist = Math.sqrt((player.x-mousex)**2 + (player.y-mousey)**2);
-    this.dx = 14*(mousex-x)/this.pdist;
-    this.dy = 14*(mousey-y)/this.pdist;
+    this.pdist = Math.sqrt((this.x-x2)**2 + (this.y-y2)**2);
+    this.dx = 14*(x2-this.x)/this.pdist;
+    this.dy = 14*(y2-this.y)/this.pdist;
       this.bullet = false;
     }
 
@@ -148,21 +155,13 @@ function Bullet_Circle(x,y,r){
    
         this.draw();
 
-
-
     }
-
-
-
-
 
 }
 
-function Enemy(){
+function Enemy(color){
     
     this.whatiszero = Math.random()-0.5;
-
-
 
     this.x = Math.random()*innerWidth;
     this.y = Math.random()*innerHeight;
@@ -182,7 +181,7 @@ function Enemy(){
         }
     this.draw = function(){
         c.beginPath();
-        c.fillStyle = '#704115'
+        c.fillStyle = color
         c.arc(this.x,this.y,15,0,6.28);
         c.fill();
     }
@@ -192,7 +191,7 @@ this.update = function(){
     this.pdist = Math.sqrt((player.x-this.x)**2 + (this.y-player.y)**2);
     this.dx = 1*(player.x-this.x)/this.pdist;
     this.dy = 1*(player.y-this.y)/this.pdist;
-      this.bullet = false;
+      
     
 
 
@@ -204,46 +203,135 @@ this.update = function(){
 }
 }
 
+function shooter(color){
+    
+    this.whatiszero = Math.random()-0.5;
+    this.timer = 120;
+    this.x = Math.random()*innerWidth;
+    this.y = Math.random()*innerHeight;
+//only spawn in the edge
+    if(this.whatiszero>0)
+    {   if(this.whatiszero>0.25)
+        this.x = 0;
+        else
+            this.x = innerWidth;
+    }
+        else{
+            if(this.whatiszero<-0.25)
+        
+            this.y = 0;
+            else
+                this.y = innerHeight;
+        }
+    this.draw = function(){
+        c.beginPath();
+        c.fillStyle = color
+        c.arc(this.x,this.y,15,0,6.28);
+        c.fill();
+    }
+
+this.update = function(){
+
+    this.pdist = Math.sqrt((player.x-this.x)**2 + (this.y-player.y)**2);
+    this.dx = 1*(player.x-this.x)/this.pdist;
+    this.dy = 1*(player.y-this.y)/this.pdist;
+    this.timer -= 1;
+    
+
+
+    this.x+=this.dx;
+    this.y+=this.dy;
+
+
+    if(this.timer<=0){
+        enemyBullets.push(new Bullet_Circle(this.x,this.y,3,player.x,player.y))
+        this.timer = 120;
+    
+    }
+   
+        this.draw();
+}
+}
+
+
 function detect(x1,y1,r1,x2,y2,r2){
     return (Math.sqrt((y2-y1)**2 + (x2-x1)**2)<r2+r1 )
 
 }
 
 
-//spawn enemies
-setInterval(() => {
- enemies.push(new Enemy())   
-}, enemytime);
-
-
-
 function animate(){
-    //clear everything
-    c.clearRect(0,0,innerWidth,innerHeight);
+    //clear everything and changes canvas size
+c.clearRect(0,0,innerWidth,innerHeight);
+
 if(!gameover)
 {
 
+//hp text
+c.fillStyle = "#CEF09D"
+c.textAlign = 'right';
+c.textBaseline = 'top';
+c.font = "30px Arial"
+c.fillText(`HP: ${player.hp}`,canvas.width -30,30)
 
-//make player
 
+
+//update player
 player.update();
 
+//enemy bullets timer
+shoot-=1;
+
+//spawn enemy
+enemytime-=1;
+
+if(enemytime <= 0){
+    if (enemyCount %3 == 0){
+ enemies.push(new shooter('#8B8000')) 
+ enemyCount+=1;
+    }
+    else{
+ enemies.push(new Enemy('#704115')) 
+ enemyCount+=1;
+}
+enemytime = 120;
+}
+
+if(shoot == 0 && enemies.length>0){
+    for(let i = 0;i<enemies.length;i++){
+        if(enemyCount%3 == 0){
+    enemyBullets.push(new Bullet_Circle(enemies[i].x,enemies[i].y,3,player.x,player.y))
+    }
+    }
+    shoot = 180;
+}
 
 
+//invincibility 
+if(is_hit >0)
+is_hit -=1;
 
 //enemy
 if(enemies.length>0){
     for(let i = 0; i<enemies.length;i++){
     enemies[i].update();
-    if (detect(player.x, player.y, 30, enemies[i].x,enemies[i].y, 15))
-    {
-    gameover = true;
+    
+
+    if (is_hit==0 && detect(player.x, player.y, 30, enemies[i].x,enemies[i].y, 15))
+    {   
+        player.hp-=1;
+        if(player.hp<=0)
+            gameover = true;
+        is_hit = 60;
 }
+
     }
 
 }
 
-
+for(let i = enemyBullets.length -1;i>=0;i--){
+    enemyBullets[i].update();
+}
 
 for(let i = bullets.length -1; i >=0 ;i--){
     
@@ -274,7 +362,6 @@ if(bullets!= [])
 
 }
 
-enemytime -=0.1;
     requestAnimationFrame(animate);
 }
 
@@ -286,7 +373,7 @@ c.fillText("GAME OVER",innerWidth/2,innerHeight/2)
 
 c.textAlign = 'center';
 c.font = "30px Arial"
-c.fillText("Restart to Play Again",innerWidth/2 ,innerHeight/2+ 50)
+c.fillText("Restart to Play Again",innerWidth/2 ,innerHeight/2+ 70)
 
 
 }
